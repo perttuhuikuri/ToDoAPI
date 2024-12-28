4eff0bc4960c826274207c6ad09197da53451ae1
perttuhuikuri <perttuhuikuri@users.noreply.github.com>
2024-12-28 20:12:27 +0000

    deploy: 4bce42a32a33f4daa86cf7cecf83754bbcb676c4

--- config.example.js
+++
@@ -1,1 +0,0 @@
-const apiBase = "YOUR_API_BASE_URL";
--- config.js
+++
@@ -1,1 +0,0 @@
-const apiBase = 'https://todoapifunction.azurewebsites.net/api/ToDoFunction?code=w0fv9msxegHVOGCY4Rddi0v7xsQqu1t9G9dBxLE3iekvAzFuAHJ5Eg%3D%3D';
